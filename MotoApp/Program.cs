b0032153852473e5c522628e5674c736af03a843
pawelwiter <pawel.witer@gmail.com>
2023-08-25 21:31:50 +0200

    Add project files.

---
+++ MotoApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using MotoApp;
+
+var stack = new MyStack<float>();
+stack.Push(item: 4.5f);
+stack.Push(item: 43);
+stack.Push(item: 333.6f);
+
+var stackCh = new Stack<char>();
+stackCh.Push(item: 'c');
+stackCh.Push(item: 'D');
+stackCh.Push(item: 'a');
+
+float sum = 0;
+
+while (stackCh.Count > 0)
+{
+    char item = stackCh.Pop();
+    Console.WriteLine( $"Item: {item}");
+    //sum += item;
+}
+
+while (stack.Count > 0)
+{
+    float item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
