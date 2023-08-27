9864c7a8fe147f64e3021903352a594e5630c8f2
pawelwiter <pawel.witer@gmail.com>
2023-08-27 12:32:20 +0200

    IRepository

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,9 +1,20 @@
-﻿using MotoApp.Entities;
+﻿using MotoApp.Data;
+using MotoApp.Entities;
 using MotoApp.Repositories;
 
 
-var employeeRepository = new GenericRepository<Employee>();
-employeeRepository.Add(new Employee { FirstName = "Paweł" });
-employeeRepository.Add(new Employee { FirstName = "Marek" });
-employeeRepository.Add(new Employee { FirstName = "Kasia" });
-employeeRepository.Save();
+//var employeeRepository = new GenericRepository<Employee>();
+//employeeRepository.Add(new Employee { FirstName = "Paweł" });
+//employeeRepository.Add(new Employee { FirstName = "Marek" });
+//employeeRepository.Add(new Employee { FirstName = "Kasia" });
+//employeeRepository.Save();
+
+var sqlRepository = new SqlRepository(new MotoAppDbContext());
+
+sqlRepository.Add(new Employee { FirstName = "Paweł" });
+sqlRepository.Add(new Employee { FirstName = "Marek" });
+sqlRepository.Add(new Employee { FirstName = "Kasia" });
+
+sqlRepository.Save();
+var emp = sqlRepository.GetById(1);
+Console.WriteLine(emp.ToString());
---
+++ MotoApp/Repositories/SqlRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace MotoApp.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using MotoApp.Entities;
+
+
+
+public class SqlRepository
+{
+    private readonly DbSet<Employee> _dbSet;
+    private readonly DbContext _dbContext;
+
+    public SqlRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<Employee>();
+    }
+
+    public Employee? GetById(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public void Add(Employee item)
+    {
+        _dbSet.Add(item);
+    }
+
+    public void Remove(Employee item)
+    {
+        _dbSet.Remove(item);
+    }
+
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+
+}
